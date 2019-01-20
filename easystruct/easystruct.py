12e2a4b2f9ec4c9164b74f9423d782590465f366
adam cushner <adam.cushner@xaxis.com>
2019-01-20 01:28:15 -0500

    small cleanup

--- easystruct/easystruct.py
+++ easystruct/easystruct.py
@@ -6,6 +6,8 @@ from struct import *
 from typing import NamedTuple, Iterable
 
 
+# TODO: use annotations instead?
+
 class StructField(NamedTuple):
     struct: Struct
     num_bytes: int
@@ -15,8 +17,7 @@ class StructField(NamedTuple):
 
     def unpack(self, byte_stream: Iterable):
         res = self.struct.unpack(bytes(islice(byte_stream, self.num_bytes)))
-        res = res[0] if len(res) == 1 else res
-        return res
+        return res[0] if len(res) == 1 else res
 
     @classmethod
     def prepare(cls, data):
@@ -30,6 +31,8 @@ class StructIterField(StructField):
 
 
 class EasyStruct:
+    """descriptor that represents a field to pack/unpack"""
+
     def __init__(self, fmt_str: str, field_cls=StructField):
         self._fmt_str = fmt_str
         self.field = field_cls(Struct(fmt_str), calcsize(fmt_str))
@@ -43,12 +46,10 @@ class EasyStruct:
 
     def pack(self):
         d = self.instance.payload[self.name]
-        print(d)
         return self.field.pack(d)
 
     def unpack(self, data):
         res = self.field.unpack(data)
-        print('fdup', res)
         self.instance.__dict__[self.name] = res
 
 
@@ -62,26 +63,25 @@ class EasyStructBase:
         self.payload = payload
 
     def pack(self):
-        return b''.join(getattr(self, name).pack() for name in self._lifx_fields())
+        return b''.join(getattr(self, name).pack() for name in self._struct_fields())
 
     @classmethod
     def unpack(cls, data: bytes):
         res = cls({})
         data = iter(data)
-        for name in cls._lifx_fields():
+        for name in cls._struct_fields():
             getattr(res, name).unpack(data)
         return res
 
     @classmethod
     @lru_cache()
-    def _lifx_fields(cls):
+    def _struct_fields(cls):
         data = chain(*(vars(c).items() for c in reversed(inspect.getmro(cls))))
         fields = [name for name, f in data if isinstance(f, EasyStruct)]
-        print(fields)
         return fields
 
     def __str__(self):
-        fields = {name: getattr(self, name) for name in self._lifx_fields()}
+        fields = {name: getattr(self, name) for name in self._struct_fields()}
         field_str = ', '.join(f'{k}={v}' for k, v in fields.items())
         return f'{type(self).__name__}({field_str})'
 
@@ -103,7 +103,6 @@ c = 1, 2, 3, 4
 m2 = M2(dict(short=c[0], color=c))
 t = m2.pack()
 t = t
-print(t)
 new_m2 = M2.unpack(t)
 print(new_m2)
 print(new_m2.short + 2)
@@ -112,6 +111,17 @@ m3 = M3(dict(short=c[0], color=c, string=b'snth'))
 print(M3.unpack(m3.pack()))
 
 from dataclasses import dataclass
+
+
 @dataclass
 class Jeb:
     a: EasyStruct('H')
+
+
+@dataclass
+class Tuse(Jeb):
+    b: EasyStruct('H')
+
+
+t = Tuse(1, 2)
+print(t.a)
