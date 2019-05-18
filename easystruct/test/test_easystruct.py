cf65f74eb838c7986e45f91db1c9d03baef7f939
acushner <sweettuse@gmail.com>
2019-05-17 20:17:58 -0400

    got bits and bytes working together

--- easystruct/test/test_easystruct.py
+++ easystruct/test/test_easystruct.py
@@ -17,6 +17,16 @@ class C(A):
     b: EasyStruct('H')
 
 
+def _create(cls, *args):
+    print(80 * '=')
+    c = cls(*args)
+    print(c)
+    print(c.pack())
+    print(cls.unpack(c.pack()))
+    print(80 * '=')
+    return c
+
+
 def _compare(s: EasyStructBase, packed):
     s_p = s.pack()
     s_u = s.unpack(packed)
@@ -38,4 +48,54 @@ def test_compound_fields():
 def test_compound_objects():
     a = A(tuple(range(40, 44)), 'abcdefghi')
     c = C(a, 65535)
+    print(c)
+    print(c.pack())
+    print(C.unpack(c.pack()))
+    return
     _compare(c, b'(\x00)\x00*\x00+\x00abcdefghi\x00\x00\x00\xff\xff')
+
+
+class Bits(EasyStructBase):
+    two_bits: EasyStruct(b'u2')
+    nine_bits: EasyStruct(b'u9')
+
+
+class MultiBits(EasyStructBase):
+    three: EasyStruct(b'u4u4u4')
+    two: EasyStruct(b'u4u12')
+    one: EasyStruct(b'u97')
+
+
+class BitsBytes(EasyStructBase):
+    s: EasyStruct('H')
+    twelve_bits: EasyStruct(b'u12')
+    seven_bits: EasyStruct(b'u7')
+    s2: EasyStruct('12s')
+
+
+class BitsBytesStr(EasyStructBase):
+    bits: Bits
+    s: EasyStruct('12s')
+    bits_bytes: BitsBytes
+
+
+bits = _create(Bits, 2, 255)
+bits_bytes = _create(BitsBytes, 4, 1000, 100, 'jebtus')
+bbs = _create(BitsBytesStr, bits, 'hello', bits_bytes)
+multi = _create(MultiBits, [1, 2, 3], [4, 5], 98273498798729873987298374987)
+
+
+def test_bits():
+    _compare(bits, b'\x9f\xe0')
+
+
+def test_bits_bytes():
+    _compare(bits_bytes, b'\x04\x00>\x8c\x80jebtus\x00\x00\x00\x00\x00\x00')
+
+
+def test_bbs():
+    _compare(bbs, b'\x9f\xe0hello\x00\x00\x00\x00\x00\x00\x00\x04\x00>\x8c\x80jebtus\x00\x00\x00\x00\x00\x00')
+
+
+def test_multibits():
+    _compare(multi, b'\x124\x00Y\xecOv\xa6"A\xc1t\xed}\xaaX')
--- playaround.py
+++ playaround.py
@@ -4,17 +4,7 @@ from easystruct import *
 import bitstruct
 from pprint import pprint
 
-from easystruct.easystruct import _StructField, _StructIterField
-
-
-class EasyBitStruct(EasyStruct):
-    @staticmethod
-    def _init_is_str(fmt_str):
-        return False
-
-    @staticmethod
-    def _init_field(fmt_str, is_str) -> _StructField:
-        return _StructIterField(bitstruct.compile(fmt_str), bitstruct.calcsize(fmt_str))
+from easystruct.easystruct import _StructField
 
 
 class H(NamedTuple):
@@ -29,6 +19,7 @@ class H(NamedTuple):
     @classmethod
     def unpacker(cls, data):
         return cls(*data)
+
 
 print(H.fmt_str)
 print(H(1, 2))
@@ -47,5 +38,98 @@ class B(Header):
     field: EasyStruct('H')
 
 
-b = B(1, 2)
-pprint(b.__dataclass_fields__)
+class Bits(EasyStructBase):
+    one_bit: EasyStruct(b'u1')
+    four_bits: EasyStruct(b'u4')
+
+
+class FiveBits(EasyStructBase):
+    one_bit: EasyStruct(b'u1')
+    four_bits: EasyStruct(b'u4')
+    five_bits: EasyStruct(b'u1u4')
+
+
+# b = Bits(1, 3)
+# print(b.pack())
+# print(Bits.unpack(b.pack()))
+
+# fb = FiveBits(1, 2, [1, 2])
+# print(fb)
+# print(len(fb.pack()))
+# print(fb.pack())
+# print(FiveBits.unpack(fb.pack()))
+
+class EightBits(EasyStructBase):
+    three_bits: EasyStruct(b'u3')
+    five_bits: EasyStruct(b'u5')
+
+
+class TenBits(EasyStructBase):
+    three_bits: EasyStruct(b'u3')
+    five_bits: EasyStruct(b'u5')
+    two_bits: EasyStruct(b'u2')
+
+
+# eb = EightBits(4, 7)
+# print(eb)
+# print(eb.pack())
+# print(EightBits.unpack(eb.pack()))
+# # print(EightBits.unpack(eb.pack()))
+#
+# tb = TenBits(4, 30, 3)
+# print(tb)
+# print(tb.pack())
+# print(TenBits.unpack(tb.pack()))
+
+
+# class Orig(EasyStructBase):
+#     shorts: EasyStruct('<4H')
+#
+#
+# tb = Orig([1, 2, 3, 1])
+# print(tb)
+# print(tb.pack())
+# print(Orig.unpack(tb.pack()))
+
+class S(EasyStructBase):
+    i: EasyStruct('<H')
+    s: EasyStruct('12s')
+
+
+s = S(4, 'jebtuse')
+print(s)
+print(s.pack())
+print(S.unpack(s.pack()))
+
+
+# class Extra(TenBits):
+#     shorts: EasyStruct('<4H')
+
+class OhGod(EasyStructBase):
+    i: EasyStruct(b'u2u4u2')
+
+
+og = OhGod([1, 2, 3])
+print(og)
+print(og.pack())
+print('unpack', OhGod.unpack(og.pack()))
+
+# tb = Extra(4, 30, 3, [1, 2, 3, 4])
+# print(tb)
+# print(tb.pack())
+# print(Extra.unpack(tb.pack()))
+#
+# class Other(EasyStructBase):
+#     t: EasyStruct(b'<H')
+
+
+# o = Other(12)
+# print(o)
+# print(o.pack())
+
+# b = B(*range(7))
+# pprint(b.__dataclass_fields__)
+# pprint(b.pack())
+# h = H(*range(4))
+# print(h)
+# # print(h.pack())
